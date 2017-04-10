eac69a4fc5dc02961cf767f2b145853d387b60c1
poilon <poilon@gmail.com>
2017-04-10 16:17:39 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,8 @@
-
 Artist.create(name: 'Alkpote') unless Artist.find_by_name('Alkpote')
 artist = Artist.find_by_name('Alkpote')
-
-songs = `curl -d 'name=Alkpote' -d 'genre=rap' 'http://genius-api.com/api/artistInfo'`
-songs.insert(0, '[')
-songs << ']'
-
+songs = "[#{`curl -d 'name=Alkpote' -d 'genre=rap' 'http://genius-api.com/api/artistInfo'`}]"
 song_names = JSON.parse(songs)
-
 lyrics_links = song_names.first['songs'].map{|e| e['link'].gsub('http://rapgenius.com', '')}
-
-
 lyrics_links.each do |link|
   html = Nokogiri::HTML(HTTParty.get(link))
   Song.create(
