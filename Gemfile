eb6d6469b844db4f74df47699a640bd4ba3697b4
poilon <poilon@gmail.com>
2017-05-05 07:34:22 +0200

    orelsan

--- Gemfile
+++ Gemfile
@@ -43,15 +43,9 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # gem 'capistrano-rails', group: :development
 
 group :development, :test do
-  # Call 'byebug' anywhere in the code to stop execution and get a debugger console
-  gem 'byebug'
-
-  # Access an IRB console on exception pages or by using <%= console %> in views
-  gem 'web-console', '~> 2.0'
-
-  # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
-  gem 'spring'
-  gem 'pry'
   gem 'better_errors'
+  gem 'byebug'
+  gem 'pry'
+  gem 'spring'
+  gem 'web-console', '~> 2.0'
 end
-
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 Genius.access_token = ENV['GENIUS_TOKEN']
 
-[14_388, 15_427].each do |artist_genius_id|
+[14_388, 15_427, 1286].each do |artist_genius_id|
   name = Genius::Artist.find(artist_genius_id).name
   Artist.create(name: name) unless Artist.find_by(name: name)
   artist = Artist.find_by(name: name)
@@ -11,6 +11,7 @@ Genius.access_token = ENV['GENIUS_TOKEN']
     html = Nokogiri::HTML(HTTParty.get(link))
     title_selector = '.song_header-primary_info-title'
     next if Song.find_by_title(html.css(title_selector).text.strip)
+    puts html.css(title_selector).text.strip
     Song.create(
       title: html.css('.song_header-primary_info-title').text.strip,
       lyrics: html.css('.lyrics p').text.gsub(/\[.*\]/, ''),
